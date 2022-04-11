6e4258f32d7d2848b9ddba299b14fd0b837e2d9e
MatthewFord17 <98977730+MatthewFord17@users.noreply.github.com>
2022-04-11 14:01:52 -0400

    SelectionSort

---
+++ src/javaapplication9/JavaApplication9.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication9;
+import java.io.*;
+/**
+ *
+ * @author mafor5845
+ */
+//public class JavaApplication9 {
+    
+    /**
+     * @param args the command line arguments
+     */
+    //public static void main(String[] args) {
+        // TODO code application logic here
+
+
+
+class U7_L5_template{
+  public static void main (String str[]) throws IOException {
+    int[] elements = {1, 6, 7, 3, 6, 4, 5, 10, 12, 9, 8};
+    
+    //Moves "boundary" of unsorted array
+    for (int j = 0; j < elements.length -1; j++)
+    {
+      //Finds the minimum element in the array
+      int minIndex = j;
+      for (int k = j + 1; k < elements.length; k++)
+      {
+        if (elements[k] < elements[minIndex])
+        {
+          minIndex = k;
+        }
+      }
+      //Swap the minimum element with the first element
+      int temp = elements[j];
+      elements[j] = elements[minIndex];
+      elements[minIndex] = temp;
+    }
+    
+    for (int num : elements)
+    {
+      System.out.println(num);
+    }
+  }
+}
